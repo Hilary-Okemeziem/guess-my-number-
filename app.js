ab9f2ef998073c2950caf320e4881c49ea2bdfa9
Lhary <hilaryokems@gmail.com>
2022-05-04 12:18:59 +0100

    second commit

--- app.js
+++ app.js
@@ -2,31 +2,79 @@ let check = document.querySelector('.check')
 let scores = document.querySelector('.number')
 let guess = document.querySelector('.guess')
 let answer = document.querySelector('h1')
+let message = document.querySelector('.message')
+let trial = document.querySelector('.score')
 
+let startGame = () => {
+    let numbers = [1,2,3,4,5]
+    let random = Math.floor(Math.random()* numbers.length)
+    scores.innerHTML = numbers[random]
 
-const randomNumberGenerator = () =>{
-    let randomNumber = Math.floor(Math.random() * 5 + 1)
-
-    guess.value = randomNumber 
+if (guess.value == null || guess.value < 1 || guess.value > 5 || guess.value == "") {
+    document.querySelector('.text').innerHTML = 'Please type a number ranging from 1-5';
+    scores.innerHTML = '?'
+    message.innerHTML = ''
+    answer.innerHTML = ''
+    trial.innerHTML = '5'
+       
+} else if (guess.value == numbers[random]) {
+    answer.innerText = 'CORRECT!!!'
+    message.innerText = 'CORRECT!!!'
+    document.body.style.backgroundColor = 'green'
+    trial.innerHTML = '5'
     
-    return randomNumber
+} else if(trial.innerText == 0){
+    message.innerText = 'Game Over'
+    answer.innerText = 'Game Over'
+}
+    
+else {
+    answer.innerText = 'Opps...Try Again' 
+    message.innerText = 'Continue guessing'
+    document.body.style.backgroundColor = 'red'
+    trial.innerText--   
+}
 }
 
-check.addEventListener('click', randomNumberGenerator)
+check.addEventListener('click', startGame)
 
-const randomNumberGenerator2 = () =>{
-    let randomNumber2 = Math.floor(Math.random() * 5 + 1)
 
-    scores.innerText = randomNumber2
+// const randomNumberGenerator = () =>{
+//     let randomNumber = Math.floor(Math.random() * 5 + 1)
 
-    if (randomNumber2 == randomNumberGenerator() ) {
-        answer.innerText = 'CORRECT!!!'
+//     guess.value = randomNumber 
     
-    } else {
-        answer.innerText = 'Opps...Try Again'    
-    }  
+//     return randomNumber
+// }
 
+// check.addEventListener('click', randomNumberGenerator)
 
-}
+// const randomNumberGenerator2 = () =>{
+//     let randomNumber2 = Math.floor(Math.random() * 5 + 1)
 
-check.addEventListener('click', randomNumberGenerator2)
+//     scores.innerText = randomNumber2
+
+//     if (randomNumber2 == randomNumberGenerator() ) {
+//         answer.innerText = 'CORRECT!!!'
+//         message.innerText = 'CORRECT!!!'
+//         document.body.style.backgroundColor = 'green'
+//         trial.innerText = '5'
+    
+//     } else if(trial.innerText == 0){
+//         message.innerText = 'Game Over'
+
+//     }else {
+//         answer.innerText = 'Opps...Try Again' 
+//         message.innerText = 'Continue guessing'
+//         document.body.style.backgroundColor = 'red'
+//         trial.innerText--   
+//     } 
+    
+    
+     
+
+    
+// }
+
+// check.addEventListener('click', randomNumberGenerator2)
+
